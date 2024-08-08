8bac868240eadd361b47c93fbcb2f7bcc9eb29d3
dkoleksandr <didenk.oleksandr@gmail.com>
2024-08-08 14:45:21 +0300

    fixed after review

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 
 async function initMongoConnection() {
   try {
-    await mongoose.connect(process.env.MONGODB_DB);
+    const MONGODB_USER = process.env.MONGODB_USER;
+    const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+    const MONGODB_URL = process.env.MONGODB_URL;
+    const MONGODB_DB = process.env.MONGODB_DB;
+
+    await mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error(error);
