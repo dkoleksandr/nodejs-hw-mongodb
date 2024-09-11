84e655b1cb6852e2602731210ea99b46535a7f5b
dkoleksandr <didenk.oleksandr@gmail.com>
2024-09-11 20:07:59 +0300

    POST for contact

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,8 @@
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import {
+  getAllContacts,
+  getContactById,
+  createContact,
+} from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res, next) => {
@@ -23,6 +27,16 @@ export const getContactController = async (req, res, next) => {
   res.send({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a contact!`,
     data: contact,
   });
 };
