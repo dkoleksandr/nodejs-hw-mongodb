e795ed6b55431517cee4da16b81a25d74734e297
dkoleksandr <didenk.oleksandr@gmail.com>
2024-10-21 22:05:04 +0300

    add Swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,3 +28,5 @@ export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'src', 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'src', 'uploads');
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
